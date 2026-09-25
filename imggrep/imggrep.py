29db136ac07ea8b23d302ebe5e3a9a8a5cc5fb1e
Ethan Lu <luethan2025@outlook.com>
2026-09-24 22:19:28 -0700

    set up phash

--- imggrep/imggrep.py
+++ imggrep/imggrep.py
@@ -1,17 +1,23 @@
 import argparse
 from pathlib import Path
 
+from PIL import Image
+from tqdm import tqdm
+
+from .hash_utils import phash
+from .os_utils import list_files
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple command-line interface.")
     parser.add_argument(
         "--input",
-        type=str,
+        type=Path,
         required=True,
         help="Path to image.",
     )
     parser.add_argument(
         "--target",
-        type=str,
+        type=Path,
         required=True,
         help="Path to target directory."
     )
@@ -31,7 +37,7 @@ def parse_args():
     if not (target.exists() and target.is_dir()):
         raise ValueError("`--target` value is not a directory")
 
-    if not args.input.endswith((".png", ".jpg", ".jpeg")):
+    if args.input.suffix.lower() not in (".png", ".jpg", ".jpeg"):
         raise ValueError("`--input` value is not an image")
 
     if args.distance <= 0:
@@ -41,6 +47,26 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    max_distance = args.distance
+    input = args.input
+    input_image_hash = phash(Image.open(input))
+    paths = list_files(args.target)
+
+    matches = []
+    for target in tqdm(
+        paths,
+        desc="Searching",
+        bar_format="{desc}: |{bar}| {percentage:3.0f}% [{elapsed}]"
+    ):
+        if input.resolve() != target.resolve():
+            try:
+                target_image_hash = phash(Image.open(target))
+                hash_difference = abs(target_image_hash - input_image_hash)
+                if hash_difference <= max_distance:
+                    matches.append(target.resolve().relative_to(Path.cwd(), walk_up=True))
+            except Exception as e:
+                pass
 
 if __name__ == "__main__":
     main()
